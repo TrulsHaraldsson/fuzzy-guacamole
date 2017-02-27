338d7e99e5649f6ca0761f6bf38dcc94b0b040ef
tedjoh-2 <tedjoh-2@student.ltu.se>
2017-02-27 17:09:47 +0100

    new update to filereader

--- filereader.py
+++ filereader.py
@@ -1,19 +1,21 @@
 import csv
-input_file_data = []
 
-with open('DSL-StrongPasswordData.csv') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        #print ", ".join(row)
-        input_file_data.append(row)
-        
-input_file_data = input_file_data[1:] # Remove the first row consisting of what each element represent.
-input_labels = [] # One label for each row of data.
-input_data = [] # Rows of data consisting with index of label.
-for row in input_file_data:
-    data = row[0]
-    data = data.split(',')
-    input_labels.append(data[0])
-    input_data.append(data[3:])
+def readFile(filename):
+    input_file_data = []
+    with open(filename) as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            input_file_data.append(row)
+            
+    input_file_data = input_file_data[1:] # Remove the first row consisting of what each element represent.
+    input_labels = [] # One label for each row of data.
+    input_data = [] # Rows of data consisting with index of label.
+    for row in input_file_data:
+        data = row[0]
+        data = data.split(',')
+        input_labels.append(data[0])
+        input_data.append(data[3:])
 
-print "label : ", input_labels[0], "\n data : ", input_data[0] # Showing test for first index of label and data.
+    return (input_labels, input_data) # return the labels and the data in two lists.
+
+print readFile('DSL-StrongPasswordData.csv')[1][399][0]
