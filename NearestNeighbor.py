a4a34738724c2d39e05b9c7712463e56b7bb9721
tedjoh-2 <tedjoh-2@student.ltu.se>
2017-03-02 09:01:06 +0100

    New update

--- NearestNeighbor.py
+++ NearestNeighbor.py
@@ -10,7 +10,7 @@ class NearestNeighbor():
         self.data = data
         self.k = k
     
-    def cross_validation(self, train_labels, train_data, folds):
+    def cross_validation(self, train_data, train_labels, folds):
         
         # By splitting up the data into folds and comparing the different votes they produce, we can get a k best suited for all folds.            
         votingbooth = np.zeros(folds)
@@ -23,6 +23,8 @@ class NearestNeighbor():
         valid_sub_labels = None
         valid_sub_data = None
         
+        # print "folds: ", folds, "label_folds: ", len(label_folds), "data_folds: ", len(data_folds)
+        
         for i in xrange(folds): # Crossvalidation i folds
             for j in xrange(folds): # Folder j == i is the validation, rest is training.
                 if(j != i):
@@ -30,51 +32,60 @@ class NearestNeighbor():
                         train_sub_labels = label_folds[j]
                         train_sub_data = data_folds[j]
                     else:
-                        train_sub_labels = np.append(train_sub_labels, label_folds[j])
-                        train_sub_data = np.append(train_sub_data, data_folds[j], axis=0)
-                    #train_sub_labels.append(validation_labels[j])
-                    #train_sub_data.append(validation_data[j])
+                        train_sub_labels = np.concatenate((train_sub_labels, label_folds[j]), axis = 0) #concatenate works fine.
+                        train_sub_data = np.concatenate((train_sub_data, data_folds[j]), axis = 0) #concatenate works fine.
+                        #train_sub_labels = np.append(train_sub_labels, label_folds[j])
+                        #train_sub_data = np.append(train_sub_data, data_folds[j]) #axis = 0
                 else: 
                     valid_sub_labels = np.array(label_folds[j])
                     valid_sub_data = np.array(data_folds[j])
+                    #print "valid_sub_length: ", len(valid_sub_data), " ", len(valid_sub_labels)
                     
-            #Now that we have train_sub and valid_sub we do the prediction.
             iterationsOfK = [1,2,4,8]
             listOfAccuracies = []
+            
             print "labels", len(valid_sub_labels), "training_labels", len(train_sub_labels)
+            
             for k in iterationsOfK:
-                self.train(train_sub_labels, train_sub_data, k)
+                self.train(train_sub_data, train_sub_labels, k)
                 prediction = self.predict(valid_sub_data)
                 predictionAccuracy = '%f' % (np.mean(prediction == valid_sub_labels) )
                 print "K: ", k, " Acc: ", predictionAccuracy
                 listOfAccuracies.append(predictionAccuracy)
-                
+                   
             votingbooth[i] = iterationsOfK[np.argmax(listOfAccuracies)]
-            
+                
         return np.mean(votingbooth)    
                 
-    def predict(self, test_data):
+
+    def predict(self, test_data): 
         num_test = test_data.shape[0]
         bar = progressbar.ProgressBar(maxval=num_test,
                                       widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
         
         # lets make sure that the output type matches the input type
-        predictions = np.zeros(num_test, dtype=self.labels.dtype)
-            
+        predictions = np.zeros(num_test, dtype = self.labels.dtype)
+        
+        print"self.data length: ", len(self.data[0]), "test_data length: ", len(test_data[0])    
+        print "type:", type(self.data[0][0]), "type:", type(test_data[0][0])
+        print "self.data: ", self.data[0][0], "test_data: ", test_data[0][0]
+        #print int(self.data[0][0] - test_data[0][0])
+        
+        #print float(self.data[0][0]) + float(test_data[0][0])
+        
         for i in xrange(num_test):
-            distances = np.linalg.norm(self.data - test_data[i, :], axis=1)  # L2
+            distances = np.linalg.norm(self.data - test_data[i,:], axis=1)  # L2
             predictions[i] = self.vote(distances)
             bar.update(i+1)
         bar.finish()
-        
+         
         return predictions
 
     def vote(self, distances):
-        """
-        The method creates the opportunity to let the distance vectors vote on who they think they are related to (e.g. p1 or p2).
-        This is because the distances is a random persons data that will be matched to either p1 or p2.
-        :param distances:
-        :return:
+        """ The method creates the opportunity to let the distance vectors vote on who they think they are related to (e.g. p1 or p2).
+            This is because the distances is a random persons data that will be matched to either p1 or p2.
+            :param distances:
+            :return:
         """
         distance = np.argsort(distances)
         votes = distance[:self.k]
