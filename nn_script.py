e0ed7c19abc339a8627b05f00722cb0643fcf583
tedjoh-2 <tedjoh-2@student.ltu.se>
2017-03-07 09:22:58 +0100

    Complete

--- nn_script.py
+++ nn_script.py
@@ -10,9 +10,21 @@ filename = "DSL-StrongPasswordData.csv"
 # Read file
 result = filereader.readFile(filename)
 
-# Extract labels and data
-labels = np.array(result[LABELS][:800])
-data = np.array(result[DATA][:800])
+# Extract labels and data from two subjects
+personOne = [0, 400]
+personTwo = [400, 800]
+
+labelsP1 = np.array(result[LABELS][personOne[0]:personOne[1]])
+dataP1 = np.array(result[DATA][personOne[0]:personOne[1]]) 
+
+labelsP2 = np.array(result[LABELS][personTwo[0]:personTwo[1]])
+dataP2 = np.array(result[DATA][personTwo[0]:personTwo[1]]) 
+
+labels = np.concatenate((labelsP1[:300], labelsP2[:300]), axis = 0)
+data = np.concatenate((dataP1[:300], dataP2[:300]), axis = 0)
+
+test_labels = np.concatenate((labelsP1[300:400], labelsP2[300:400]), axis = 0)
+test_data = np.concatenate((dataP1[300:400], dataP2[300:400]), axis = 0)
 
 # Shuffle the data
 rng_state = np.random.get_state()
@@ -23,7 +35,14 @@ np.random.shuffle(data)
 # Train NN
 nn = NearestNeighbor()
 #print "data length:", len(data),"\nlabel length:", len(labels)
-print type(data)," ", type(labels)
+#print type(data)," ", type(labels)
 k = nn.cross_validation(data, labels, 4)
+print "the highest value k is equal to :", k, "for testing."
 
-print "the highest value k is equal to :", k
+# Time to test out test_data/labels
+nn.train(data, labels, k)
+prediction = nn.predict(test_data)
+predictionAccuracy = '%f' % (np.mean(prediction == test_labels) )
+print"Test complete!\nThe accurracy is : ", predictionAccuracy
+
+
