9a927f4c9abba7d5b25ea19ccec57395dfcfb431
tedjoh-2 <tedjoh-2@student.ltu.se>
2017-03-07 09:21:34 +0100

    Complete

--- NearestNeighbor.py
+++ NearestNeighbor.py
@@ -3,25 +3,32 @@ import numpy as np
 from collections import Counter
 
 class NearestNeighbor():
+    #Empty __init__ 
     def __init__(self):
         pass
     
-    def train(self, data, labels, k): # Store the training data that will be matched from validation data.
+   
+    def train(self, data, labels, k): 
+        """ Train specific data, labels and k-value.
+            :param data:
+            :param labels:
+            :param k:
+        """
         self.labels = labels
         self.data = data
         self.k = k
     
+    
     def cross_validation(self, train_data, train_labels, folds):
-        
-        # By splitting up the data into folds and comparing the different votes they produce, we can get a k best suited for all folds.            
+        """ Cross-validation for training_data, training_labels and for certain folds.
+            :param train_data:
+            :param train_labels:
+            :param folds:
+            :return highest accuracy:
+        """
+        #Split labels and data into folds.
         label_folds = np.split(train_labels, folds)
         data_folds = np.split(train_data, folds)
-        
-        # then to do the cross validation of training and validation folds and get all the best accuracies.
-        #train_sub_labels = None
-        #train_sub_data = None
-        #valid_sub_labels = None
-        #valid_sub_data = None
         
         #accuracies
         votingbooth = []
@@ -29,14 +36,14 @@ class NearestNeighbor():
         #Test different k-values
         iterationsOfK = [1,2,3,4,5,6,7,8]
         
-        # print "folds: ", folds, "label_folds: ", len(label_folds), "data_folds: ", len(data_folds)
-        for i in xrange(folds): # Crossvalidation i folds
+        #merge sub-folds into training sets and validation sets.
+        for i in xrange(folds): 
             train_sub_labels = None
             train_sub_data = None
             valid_sub_labels = None
             valid_sub_data = None
         
-            for j in xrange(folds): # Folder j == i is the validation, rest is training.
+            for j in xrange(folds): 
                 
                 if(j != i):
                     if train_sub_labels is None:
@@ -48,18 +55,20 @@ class NearestNeighbor():
                 else: 
                     valid_sub_labels = np.array(label_folds[j])
                     valid_sub_data = np.array(data_folds[j])
-                    #print "valid_sub_length: ", len(valid_sub_data), " ", len(valid_sub_labels)
-                   
+            
+            #Stores a list of accuracies for each iteration, so it can be calculated in votingbooth's best accuracy.       
             listOfAccuracies = []
             
             print "labels", len(valid_sub_labels), "training_labels", len(train_sub_labels)
             
+            #Make a prediction for each iteration.
             for k in iterationsOfK:
                 self.train(train_sub_data, train_sub_labels, k)
                 prediction = self.predict(valid_sub_data)
                 predictionAccuracy = '%f' % (np.mean(prediction == valid_sub_labels) )
                 print "K: ", k, " Acc: ", predictionAccuracy
-                listOfAccuracies.append(predictionAccuracy)       
+                listOfAccuracies.append(predictionAccuracy)   
+                    
             votingbooth.append(listOfAccuracies)
         
         print "All the tests are now done!\nThe best results will now be calculated for best K value!"
@@ -67,32 +76,32 @@ class NearestNeighbor():
         highestAccuracy = 0
         highestIteration = None
         
-        for i in iterationsOfK: # adding fold accuracys to see total best value of k.
-            #print "i : ", i, " out of : ", len(iterationsOfK)
-            testAccuracy = float(votingbooth[0][i-1]) + float(votingbooth[1][i-1]) + float(votingbooth[2][i-1]) 
+        #Calculate the best k-value by adding votingbooth's values together for each fold.
+        for i in iterationsOfK: 
+            
+            #4 folds add 4 values.
+            testAccuracy = float(votingbooth[0][i-1]) + float(votingbooth[1][i-1]) + float(votingbooth[2][i-1]) + float(votingbooth[3][i-1])
             print "testAccuracy of the folds are : ", testAccuracy
             if(highestAccuracy < testAccuracy): 
                 highestAccuracy = testAccuracy
                 highestIteration = i
-                #print "new highest accuracy index k ", i
+               
         return highestIteration   
                 
-
-    def predict(self, test_data): 
-        num_test = test_data.shape[0]
+    
+    def predict(self, vData): 
+        """Predict method that actually measures the distances between trained data and the validation data.
+           :param vData: 
+           :return prediction:
+        """
+        num_test = vData.shape[0]
         #bar = progressbar.ProgressBar(maxval=num_test,
         #                              widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
         
-        # lets make sure that the output type matches the input type
         predictions = np.zeros(num_test, dtype = self.labels.dtype)
         
-        #print"self.data length: ", len(self.data[0]), "test_data length: ", len(test_data[0])    
-        #print "type:", type(self.data[0][0]), "type:", type(test_data[0][0])
-        #print "self.data: ", self.data[0][0], "test_data: ", test_data[0][0]
-        #print float(self.data[0][0]) + float(test_data[0][0])
-        
         for i in xrange(num_test):
-            distances = np.linalg.norm(self.data - test_data[i,:], axis=1)  # L2
+            distances = np.linalg.norm(self.data - vData[i,:], axis=1)  # L2
             predictions[i] = self.vote(distances)
             #bar.update(i+1)
         #bar.finish()
@@ -103,12 +112,11 @@ class NearestNeighbor():
         """ The method creates the opportunity to let the distance vectors vote on who they think they are related to (e.g. p1 or p2).
             This is because the distances is a random persons data that will be matched to either p1 or p2.
             :param distances:
-            :return:
+            :return label vote:
         """
         
         distance = np.argsort(distances)
         votes = distance[:self.k]
-        #print "votes: ",votes, "with length : ", len(votes)
         prediction = {} # There are two alternatives for the vote, person 1 or person 2.
         for i in votes:
             label = self.labels[i]
@@ -117,8 +125,8 @@ class NearestNeighbor():
         cnt = Counter()
         for p in prediction:
             cnt[p] += 1
-        maxVote = cnt.most_common(1)
-        #print "the person with most votes is : ", maxVote, " - ", maxVote[0][0], " - ", self.labels[maxVote[0][0]]
+        maxVote = cnt.most_common(1) #One is always best. Or at least got most votes (think Trump)..
+        
         return self.labels[maxVote[0][0]]
     
     
