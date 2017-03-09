5ce1a8709b4daf4dfed0a75693887bc560a49bbc
Truls Haraldsson <truhar-3@student.ltu.se>
2017-03-09 16:50:56 +0100

    Started with perceptron

---
+++ perception_script.py
@@ -0,0 +1,68 @@
+'''
+Created on Dec 1, 2016
+
+@author: eao
+'''
+from utilities import Utilities as util
+from perceptron import *
+from datahelper import *
+
+
+# Just some constants
+LABELS = 0
+DATA = 1
+filename = "DSL-StrongPasswordData.csv"
+
+dm = DataManager()
+
+# Read file
+dm.pre_process(filename)
+
+# Extract labels and data from two subjects
+
+p1 = dm.get_subject(1)
+p2 = dm.get_subject(3)
+
+labels_p1 = p1[0]
+data_p1 = p1[1]
+
+labels_p2 = p2[0]
+data_p2 = p2[1]
+
+# Training data
+labels = np.concatenate((labels_p1[:300], labels_p2[:300]), axis=0)
+data = np.concatenate((data_p1[:300], data_p2[:300]), axis=0)
+
+# Test data
+test_labels = np.concatenate((labels_p1[300:400], labels_p2[300:400]), axis=0)
+test_data = np.concatenate((data_p1[300:400], data_p2[300:400]), axis=0)
+
+# Shuffle the data
+rng_state = np.random.get_state()
+np.random.shuffle(labels)
+np.random.set_state(rng_state)
+np.random.shuffle(data)
+
+
+def prepare_for_back_propagation(batch_size, train_data, train_labels, valid_data, valid_labels):
+
+    print ("Creating data...")
+    batched_train_data, batched_train_labels = util.create_batches(train_data, train_labels,
+                                                                   batch_size,
+                                                                   create_bit_vector=True)
+    batched_valid_data, batched_valid_labels = util.create_batches(valid_data, valid_labels,
+                                                                   batch_size,
+                                                                   create_bit_vector=True)
+    print ("Done!")
+
+    return batched_train_data, batched_train_labels,  batched_valid_data, batched_valid_labels
+
+batch_size = 1
+
+train_data, train_labels, valid_data, valid_labels = prepare_for_back_propagation(batch_size, data, labels, test_data, test_labels)
+
+#mlp = MultiLayerPerceptron(layer_config=[784, 100, 100, 10], batch_size=batch_size)
+mlp = MultiLayerPerceptron(layer_config=[31, 100, 100, 2], batch_size=batch_size)
+
+mlp.evaluate(train_data, train_labels, valid_data, valid_labels,
+             eval_train=True)
---
+++ utilities.py
@@ -0,0 +1,52 @@
+'''
+Created on 3 Jan 2017
+
+@author: eao
+'''
+import numpy as np
+import os
+import sys
+
+def label_to_bit_vector(labels, nbits):
+    bit_vector = np.zeros((labels.shape[0], nbits))
+    for i in range(labels.shape[0]):
+        bit_vector[i, labels[i]] = 1.0
+
+    return bit_vector
+
+
+class Utilities(object):
+
+    @staticmethod
+    def test():
+
+        # dir_path = os.path.dirname(os.path.realpath(__file__))
+        # dir_path = os.getcwd()
+        # print("Util.Test is done. DIR=", dir_path)
+        return
+
+    @staticmethod
+    def exit_with_error(err_str):
+        print >> sys.stderr, err_str
+        sys.exit(1)
+
+    @staticmethod
+    def create_batches(data, labels, batch_size, create_bit_vector=False):
+        N = data.shape[0]
+        if N % batch_size != 0:
+            print "Warning in create_minibatches(): Batch size {0} does not " \
+                  "evenly divide the number of examples {1}.".format(batch_size,N)
+        chunked_data = []
+        chunked_labels = []
+        idx = 0
+        while idx + batch_size <= N:
+            chunked_data.append(data[idx:idx+batch_size, :])
+            if not create_bit_vector:
+                chunked_labels.append(labels[idx:idx+batch_size])
+            else:
+                bit_vector = label_to_bit_vector(labels[idx:idx+batch_size], 10)
+                chunked_labels.append(bit_vector)
+
+            idx += batch_size
+
+        return chunked_data, chunked_labels
